f960ff45cfd1472d575e5c8d3a76acf32b27b805
Annie Pulcherie <roudan@gmail.com>
2023-08-10 21:50:35 -0700

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *File: main.h
+ *Auth: Annie
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array__range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
